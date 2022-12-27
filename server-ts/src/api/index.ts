807d04e4f67bf280b8ad306457779d427f8c94f5
ณัฐนนท์ ชุมฤทธิ์ <nattanon0280@gmail.com>
2022-12-27 11:36:58 +0700

    แทรก authMiddleware ก่อนเรียก router

--- server-ts/src/api/index.ts
+++ server-ts/src/api/index.ts
@@ -1,7 +1,8 @@
 import Router from "koa-router";
 import announcement from './announcement'
+import { authMiddleware } from "../auth";
 
 const apiRouter = new Router()
-apiRouter.use('/api/announcement',announcement.routes())
+apiRouter.use('/api/announcement',authMiddleware,announcement.routes())
 
 export default apiRouter
