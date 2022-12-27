3b6b459d8ab9f327c3e85737c3667bc99ebcbb72
ณัฐนนท์ ชุมฤทธิ์ <nattanon0280@gmail.com>
2022-12-27 11:39:53 +0700

    เรียก funciton initSsoCret ดึง public key

--- server-ts/src/index.ts
+++ server-ts/src/index.ts
@@ -5,6 +5,7 @@ import cors from '@koa/cors'
 import apiRouter from "./api"
 import loadFixtures from "./fixtures"
 import appConfig from "./config"
+import { initSsoCert } from "./auth"
 
 
 
@@ -18,6 +19,7 @@ app.use(apiRouter.routes());
 
 (async () => {
     await loadFixtures(appConfig.clearDataBeforeLodingFixture)
+    await initSsoCert()
     app.listen(8000)
     console.log('Server is ready at port 8000')
 })();
