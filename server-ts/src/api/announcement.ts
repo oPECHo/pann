a2a333121666603f5d2d0cde4f3d85b9be82d5ad
ณัฐนนท์ ชุมฤทธิ์ <nattanon0280@gmail.com>
2022-12-27 11:33:12 +0700

    ใช้ AuthData ใน announcement (access control)

--- server-ts/src/api/announcement.ts
+++ server-ts/src/api/announcement.ts
@@ -1,17 +1,19 @@
-import Router from "koa-router"
-import db from "../db"
-
+import { AuthData } from 'auth'
+import Router from 'koa-router'
+import db from '../db'
 const router = new Router()
-router
-    .get('/', async (ctx,next) => {
-        let query = db('announcement').select('*')
-        if (ctx.request.query['keyword']) {
-            const keyword = String(ctx.request.query['keyword'])
-            query = query.where((it) => {it.where('topic','like', `%${keyword}`).orWhere('description','like',`%${keyword}`)})
-        }
-        ctx.body = await query.orderBy('id','desc')
-    })
 
+router
+  .get('/', async (ctx, next) => {            
+    const authData = ctx.state.authData as AuthData
+    let query = db('announcement').select('*').where({ 'announcement.userCode': authData.username })
+    if (ctx.request.query['keyword']) {
+      const keyword = String(ctx.request.query['keyword'])
+      query = query.where((it) => {it.where('topic', 'like', `%${keyword}%`).orWhere('description', 'like', `%${keyword}%`)})
+    }
+    ctx.body = await query.orderBy('id', 'desc')
+  })
+ 
 
 
 export default router
