05f7a8a0f7a0ddfffea06d2d31b803473acdc21b
ณัฐนนท์ ชุมฤทธิ์ <nattanon0280@gmail.com>
2022-12-28 20:45:12 +0700

    แทรก authMiddleware ก่อนเรียก router userResult

--- server-ts/src/api/index.ts
+++ server-ts/src/api/index.ts
@@ -5,6 +5,6 @@ import { authMiddleware } from "../auth";
 
 const apiRouter = new Router()
 apiRouter.use('/api/announcement',authMiddleware,announcement.routes())
-apiRouter.use('/api/userResult',userResult.routes())
+apiRouter.use('/api/userResult',authMiddleware,userResult.routes())
 
 export default apiRouter
