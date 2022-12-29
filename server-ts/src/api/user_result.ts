fc5f4ce494499d1ca19971e03082de5dfbb7a6e1
ณัฐนนท์ ชุมฤทธิ์ <nattanon0280@gmail.com>
2022-12-29 08:14:43 +0700

    เพิ่ม method ใน user_result ให้ครบ

--- server-ts/src/api/user_result.ts
+++ server-ts/src/api/user_result.ts
@@ -12,6 +12,10 @@ const makeQuery = () => db('userResult').select(
     'announcement.remarkIfNegative as announcementRemarkIfNegative',
     'announcement.pubDateTime as announcementPubDateTime'
   ).leftJoin('announcement', 'userResult.announcementId', 'announcement.id')
+
+  const updateUserResult = (id: number, userCode: string, data: any) => {
+    return db('userResult').where({ id, userCode }).update(data)
+  }
   
 router
     .get('/', async (ctx,next) => {
@@ -32,5 +36,38 @@ router
         const userResults = await query.orderBy('id','desc')
         ctx.body = userResults.map(it => nestObject(it, 'announcement'))
     })
-
+    .get('/:id/markAsViewed', async (ctx, next) => {
+        const id = parseInt(ctx.params.id)
+        const authData = ctx.state.authData as AuthData
+        const viewDateTime = new Date()
+        const rowUpdated = await updateUserResult(id, authData.username, { viewDateTime })
+        if(rowUpdated == 0){
+          ctx.response.status = 404
+          return
+        }
+        ctx.body = {statusCode: 1, viewDateTime}
+    })
+    .get('/:id/acknowledge', async (ctx, next) => {
+        const id = parseInt(ctx.params.id)
+        const authData = ctx.state.authData as AuthData
+        const ackDateTime = new Date()
+        const rowUpdated = await updateUserResult(id, authData.username, { ackDateTime })
+        if(rowUpdated == 0){
+          ctx.response.status = 404
+          return
+        }
+        ctx.body = {statusCode: 1, ackDateTime}
+    })
+    .get('/:id/pin/:value', async (ctx, next) => {
+        const id = parseInt(ctx.params.id)
+        const authData = ctx.state.authData as AuthData
+        const isPinned = ctx.params.value == '1'
+        const rowUpdated = await updateUserResult(id, authData.username, { isPinned })
+        if(rowUpdated == 0){
+          ctx.response.status = 404
+          return
+        }
+        ctx.body = {statusCode: 1, isPinned}
+    })
+    
 export default router
