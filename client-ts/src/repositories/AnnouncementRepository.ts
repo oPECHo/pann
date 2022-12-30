6e6c1fa0303fdcc39df17fe9a15c4f8427fcc87d
ณัฐนนท์ ชุมฤทธิ์ <nattanon0280@gmail.com>
2022-12-30 17:22:59 +0700

    AnnouncementRepositort method getAll

--- client-ts/src/repositories/AnnouncementRepository.ts
+++ client-ts/src/repositories/AnnouncementRepository.ts
@@ -1,10 +1,17 @@
 import Announcement from "../models/Announcement";
 import { IRepository } from "./IRepository";
+import config, { ax } from "../config";
+
+export interface AnnouncementFilter {
+    keyword?: string
+}
 
 export class AnnouncementRepository implements IRepository<Announcement> {
-    async getAll(): Promise<Announcement[] | null> {
-        return [
-            { id: 1, topic: '240-124 Midterm 1/2566',description: 'คะแนน Assignment ชิ้นที่ 1',remarkIfPositive:'1',remarkIfNegative:'0',pubDateTime:new Date('2022-12-21 10:30:00'), userCode: 'suthon.s'}
-        ]
+    urlPrefix = config.remoteRepositoryUrlPrefix
+    async getAll(filter: AnnouncementFilter): Promise<Announcement[] | null> {
+        const params = {...filter}
+        const resp = await ax.get<Announcement[]>(`${this.urlPrefix}/announcement`, { params })
+        return resp.data
     }
 }
+
