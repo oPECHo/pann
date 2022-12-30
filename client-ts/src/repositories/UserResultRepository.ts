02fe7c8978faee3f39b7cd5d3e0d2eb16f6620c6
ณัฐนนท์ ชุมฤทธิ์ <nattanon0280@gmail.com>
2022-12-30 16:52:05 +0700

    สร้าง UserResultRepository (Mock)

--- client-ts/src/repositories/UserResultRepository.ts
+++ client-ts/src/repositories/UserResultRepository.ts
@@ -1,10 +1,18 @@
-import { IRepository } from "./IRepository";
-import UserResult from "../models/UserResult";
+import { IRepository } from "./IRepository"
+
+export default interface UserResult{
+    id: number
+    announcementId: number
+    result: string
+    remark: string
+    updateDateTime: Date
+    userCode: string
+  }
 
 export class UserResultRepository implements IRepository<UserResult> {
     async getAll(): Promise<UserResult[] | null> {
         return [
-            { id: 1, announcementId: 1, result: '24.5', remark: 'โดนหักคะแนนจากการเข้าสอบสาย', updateDateTime: '2022-09-07 09:12:31', userCode: '6510110150'}
+            { id: 1,announcementId: 1, result: '24.5', remark: 'โดนหักคะแนนจากการเข้าสอบสาย', updateDateTime: new Date('2022-09-07 09:12:31'), userCode: '6510110150'}
         ]
     }
 }
