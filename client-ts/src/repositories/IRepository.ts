dd635d1d2c253b74bfcee863d296d1e1e0bd314c
ณัฐนนท์ ชุมฤทธิ์ <nattanon0280@gmail.com>
2022-12-31 10:36:00 +0700

    IRepository ครบ 5 method

--- client-ts/src/repositories/IRepository.ts
+++ client-ts/src/repositories/IRepository.ts
@@ -1,4 +1,8 @@
 export interface IRepository<T> { 
-    getAll(filter: any|undefined): Promise<T[] | null>; 
+    getAll(filter: any|undefined): Promise<T[] | null>;
+    get(id: string|number): Promise<T | null>;
+    create(entity: Partial<T>): Promise<T | null>;
+    update(entity: Partial<T>): Promise<T | null>;
+    delete(id: string|number): Promise<void>; 
 }
  
