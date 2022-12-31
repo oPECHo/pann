1b29dfec28d995bbe83ae0294c5f6d01f1db14d9
ณัฐนนท์ ชุมฤทธิ์ <nattanon0280@gmail.com>
2022-12-31 11:45:13 +0700

    AnnouncementRepository 5 method

--- client-ts/src/repositories/AnnouncementRepository.ts
+++ client-ts/src/repositories/AnnouncementRepository.ts
@@ -8,10 +8,26 @@ export interface AnnouncementFilter {
 
 export class AnnouncementRepository implements IRepository<Announcement> {
     urlPrefix = config.remoteRepositoryUrlPrefix
+    
     async getAll(filter: AnnouncementFilter): Promise<Announcement[] | null> {
-        const params = {...filter}
-        const resp = await ax.get<Announcement[]>(`${this.urlPrefix}/announcement`, { params })
-        return resp.data
+      const params = {...filter}
+      const resp = await ax.get<Announcement[]>(`${this.urlPrefix}/announcement`, { params })
+      return resp.data
     }
-}
+    async get(id: string|number): Promise<Announcement | null> {
+      const resp = await ax.get<Announcement>(`${this.urlPrefix}/announcement/${id}`)
+      return resp.data
+    }
+    async create(entity: Partial<Announcement>): Promise<Announcement | null> {
+      const resp = await ax.post<Announcement>(`${this.urlPrefix}/announcement`, entity)
+      return resp.data
+    }
+    async update(entity: Partial<Announcement>): Promise<Announcement | null> {
+      const resp = await ax.put<Announcement>(`${this.urlPrefix}/announcement/${entity.id}`, entity)
+      return resp.data
+    }
+    async delete(id: string|number): Promise<void> {
+      await ax.delete<void>(`${this.urlPrefix}/announcement/${id}`)
+    }
+  }
 
