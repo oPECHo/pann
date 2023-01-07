ac7bf4abf464a5d76ca2822fc5ea51f9053d65f0
ณัฐนนท์ ชุมฤทธิ์ <nattanon0280@gmail.com>
2023-01-07 21:33:00 +0700

    Fix page login

--- client-ts/src/pages/login.tsx
+++ client-ts/src/pages/login.tsx
@@ -1,64 +1,63 @@
-import { useEffect } from 'react';
-import { Button } from '@mui/material';
-import { Box } from '@mui/system';
-import { Login as LoginIcon } from '@mui/icons-material';
+import { Button } from '@mui/material'
+import { Box } from '@mui/system'
+import { Login as LoginIcon } from '@mui/icons-material'
 import { useAuth } from "react-oidc-context";
-import { Navigate, useLocation } from 'react-router-dom';
-import { useAppCtx } from '../AppProvider';
+import { useAppCtx } from "../AppProvider";
+import { Navigate ,useLocation } from 'react-router-dom'
+import { useEffect } from "react";
 
 function Login() {
 
-  const { userInfo, action } = useAppCtx();
-  const auth = useAuth();
-  const location = useLocation();
+    const { userInfo, action } = useAppCtx()
+    const auth = useAuth()
+    const location = useLocation()
 
-  console.log('rendering..... login', auth.user)
-  useEffect(() => {
+    console.log('rendering..... login', auth.user)
+    useEffect(() => {
+        if(auth.isAuthenticated) {
+            setTimeout(() => {
+                action.setUserInfo({
+                    ready: true,
+                    username: auth.user?.profile.preferred_username,
+                    displayName: auth.user?.profile.given_name + ' ' + auth.user?.profile.family_name
+                })
+            },1000)
+        }
+    },[auth, userInfo.ready , action])
+
+    switch (auth.activeNavigator) {
+        case "signinSilent":
+            return <div>Signing you in...</div>
+        case "signinRedirect":
+            return <div>Signing you out...</div>
+    }
+
+    if (auth.isLoading){
+        return <div>Loading...</div>
+    }
+
+    if (auth.error) {
+        return <div>Oops... {auth.error.message}</div>
+    }
+
     if (auth.isAuthenticated) {
-      setTimeout(() => {
-        action.setUserInfo({
-          ready: true,
-          username: auth.user?.profile.preferred_username,
-          displayName: auth.user?.profile.given_name + ' ' + auth.user?.profile.family_name
-        })
-      }, 1000)
+        if (userInfo.ready) {
+            const backTo = location.state?.bactTo || '/home'
+            return (
+                <Navigate to={backTo} replace />
+            )
+        } else {
+            return <div>Waiting for whoami </div>
+        }
     }
-  }, [auth, userInfo.ready])
+    return (
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: 500}}>
+            <Button variant='contained' sx={{fontSize: 'large'}} onClick={() => void auth.signinRedirect()}>
+                <LoginIcon sx={{ mr: 1}}/>
+                Log in
+            </Button>       
+        </Box>
+    )
+}
 
-  switch (auth.activeNavigator) {
-    case "signinSilent":
-      return <div>Signing you in...</div>;
-    case "signoutRedirect":
-      return <div>Signing you out...</div>;
-  }
-
-  if (auth.isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (auth.error) {
-    return <div>Oops... {auth.error.message}</div>;
-  }
-
-  if (auth.isAuthenticated) {
-    if (userInfo.ready) {
-      const backTo = location.state?.backTo || '/home'
-      return (
-        <Navigate to={backTo} replace />
-      );
-    } else {
-      return <div>Waiting for whoami</div>;
-    }
-  }
-
-  return (
-    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: 500 }}>
-      <Button variant='contained' sx={{ fontSize: 'large' }} onClick={() => void auth.signinRedirect()}>
-        <LoginIcon sx={{ mr: 1 }} />
-        Log in
-      </Button>
-    </Box>
-  );
-};
-
-export default Login;
+export default Login
